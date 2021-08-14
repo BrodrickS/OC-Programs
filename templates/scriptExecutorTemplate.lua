5dfe234f502ed2e4cdfcabd0ca87eed527b7a3a2
Brodrick Starr <brodrickstarr@gmail.com>
2021-08-14 11:36:41 -0400

    Removed Unneeded Refs in Executor

--- templates/scriptExecutorTemplate.lua
+++ templates/scriptExecutorTemplate.lua
@@ -1,9 +1,3 @@
--- Open Computers dependencies
-local packInj = require("oxidePipit.core.packageInjector")
-
--- Oxide Pipit dependencies
-local cliOptionsFormat = require("OxidePipit.core.cli.cliOptionsFormat")
-
 -- Script execution
 local script = require("templates.scriptTemplate") 
 script:execute(...)
